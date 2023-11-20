26f9eb6c14758a84b697afbc5f160fe84b025560
LukaTamazato <luka.tamazato@sptech.school>
2023-11-20 13:11:48 -0300

    Adicionando script e modelo de banco de dados

---
+++ Bd/ScriptDragao.sql
@@ -0,0 +1,26 @@
+create database ProjetoIndividual20232;
+use ProjetoIndividual20232;
+
+create table dragao(
+idDragao int primary key auto_increment,
+tipo int,
+elemento int
+)auto_increment = 1000;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(50),
+senha varchar(45),
+fkDragao int,
+constraint fkDragaoUsuario foreign key (fkDragao)
+references dragao(idDragao)
+);
+
+create table titulo(
+idTitulo int primary key auto_increment,
+titulo varchar(50),
+fkUsuario int,
+constraint fkTituloUsuario foreign key (fkUsuario)
+references usuario(idUsuario)
+);
