1e3fbb90b51a0003347d4a5cb24de821e5c7371f
LukaTamazato <luka.tamazato@sptech.school>
2023-11-23 14:04:45 -0300

    Adicionando API ao projeto

---
+++ API/src/models/usuarioModel.js
@@ -0,0 +1,34 @@
+var database = require("../database/config")
+
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+    var instrucao = `
+        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucao
+function cadastrar(nome, email, senha, tipoDragao, titulo) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+
+    // var instrucao = `SELECT * from dragao;`
+    // var resultadoDragao = await database.executar(instrucao)
+    // '${resultadoDragao[resultadoDragao.length - 1].idDragao}'
+    var instrucao = `
+        INSERT INTO dragao  (tipoDragao) VALUES('${tipoDragao}');
+        INSERT INTO usuario (nome,email,senha,fkDragao)('${nome}', '${email}', '${senha}',(SELECT max(idDragao) from dragao) );
+        INSERT INTO titulo VALUES (${titulo},(SELECT max(idUsuario) from usuario));
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
